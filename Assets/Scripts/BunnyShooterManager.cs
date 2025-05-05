b75d4c3b4daa5abc81a55113a8d9b3c9bedb1e52
chaowat.phon <chaowat.phon@bumail.net>
2025-05-05 22:34:58 +0700

    Update Script ShootManager

--- Assets/Scripts/BunnyShooterManager.cs
+++ Assets/Scripts/BunnyShooterManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class BunnyShooterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public LineRenderer lineRenderer;
+    public Transform shootPoint; // จุดยิง (ต้นทางของกระต่าย)
+    public float forceMultiplier = 10f;
+    public int lineSegmentCount = 30;
+    public float timeStep = 0.1f;
+
+    private Camera cam;
+    private bool isDragging;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        lineRenderer.positionCount = lineSegmentCount;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            lineRenderer.enabled = false;
+        }
+
+        if (isDragging)
+        {
+            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (Vector2)(mousePos - shootPoint.position);
+            direction.Normalize();
+
+            ShowTrajectory(shootPoint.position, direction * forceMultiplier);
+        }
+    }
+
+    void ShowTrajectory(Vector2 startPos, Vector2 initialVelocity)
+    {
+        lineRenderer.enabled = true;
+
+        Vector2 gravity = Physics2D.gravity;
+        for (int i = 0; i < lineSegmentCount; i++)
+        {
+            float t = i * timeStep;
+            Vector2 point = startPos + initialVelocity * t + 0.5f * gravity * t * t;
+            lineRenderer.SetPosition(i, point);
+        }
     }
 }
